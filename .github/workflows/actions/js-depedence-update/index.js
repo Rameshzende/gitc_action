3a36343a37004ab59046cb7430a2c8f5c69d9685
Rameshzende <jhande00@gmail.com>
2024-07-08 13:53:50 +0530

    fix: updaed the error status in js custom action

--- .github/workflows/actions/js-depedence-update/index.js
+++ .github/workflows/actions/js-depedence-update/index.js
@@ -5,14 +5,35 @@ const github = require('@actions/github');
 const validateBranchName = ({ branchName }) => (/^[a-zA-Z0-9_\-\.\/]+$/.test(branchName))
 const validateDir = ({ dirName }) => (/^[a-zA-Z0-9_\-\/]+$/.test(dirName))
 
+const setUpLogger = ({ debug, prefix } = {debug: false, prefix: ''}) => ({
+    debug: (message) => {
+      if(debug) {
+        core.info(`${prefix}${prefix ? '' : 'DEBUG: '}${message}`)
+      }
+    },
+    info: (message) => {
+        core.info(`${prefix}${prefix ? '' : 'INFO: '}${message}`)
+    },
+    error: (message) => {
+      core.error(`${prefix}${prefix ? '' : 'ERROR: '}${message}`)
+    }
+})
+
+  
+const setUpGit = async () => {
+    await exec.exec('git config --global user.email "gh-automation@email.com"')
+    await exec.exec('git config --global user.name "gh-automation"')
+}
+
 
 async function run() {
     const baseBranch = core.getInput('base-branch', { required: true })
-    const targeBranch = core.getInput('target-branch', { required: true })
+    const headBranch = core.getInput('head-branch', { required: true })
     const githubToken = core.getInput('gh-token', { required: true })
     const workingDir = core.getInput('working-directory', { required: true })
     const debug = core.getBooleanInput('debug')
-
+    const logger = setUpLogger({ debug, prefix: '[js dependency update]' })
+  
 
     const commonExecOpts = {
         cwd: workingDir
@@ -20,13 +41,15 @@ async function run() {
 
     core.setSecret(githubToken)
 
+    logger.debug(`validating inputs: ${baseBranch}`)
+
     if (!validateBranchName({ branchName: baseBranch })) {
         core.setFailed('Invalid base branch name. branch name should include only alphabets, numbers, _, -, ., /')
         return
     }
 
-    if (!validateBranchName({ branchName: targeBranch })) {
-        core.setFailed('Invalid target name. branch name should include only alphabets, numbers, _, -, ., /')
+    if (!validateBranchName({ branchName: headBranch })) {
+        core.setFailed('Invalid head name. branch name should include only alphabets, numbers, _, -, ., /')
         return
     }
 
@@ -35,9 +58,11 @@ async function run() {
         return
     }
 
-    core.info(`Base Branch: ${baseBranch}`)
-    core.info(`Target Branch: ${targeBranch}`)
-    core.info(`working directory: ${workingDir}`)
+    logger.debug(`Base Branch: ${baseBranch}`)
+    logger.debug(`head Branch: ${headBranch}`)
+    logger.debug(`working directory: ${workingDir}`)
+
+    logger.debug('checking for updates')
 
     await exec.exec('npm update', [], { ...commonExecOpts })
 
@@ -50,38 +75,39 @@ async function run() {
     );
 
     if (gitStatus.stdout.length > 0) {
-        core.info('Changes detected in package*.json files')
-        await exec.exec('git config --global user.email "gh-automation@email.com"')
-        await exec.exec('git config --global user.name "gh-automation"')
-
+        logger.debug('There are updated available')
+        logger.debug('Setting up git')
+        
+        setUpGit()
+        logger.debug('Comminting and Pushing the package*.json files')
         await exec.exec(`git checkout -b ${targeBranch}`, [], { ...commonExecOpts })
         await exec.exec(`git add package.json package-lock.json`, [], { ...commonExecOpts })
         await exec.exec(`git commit -m "chore: update dependencies"`, [], { ...commonExecOpts })
         await exec.exec(`git push -u origin ${targeBranch} --force`, [], { ...commonExecOpts })
-
+       
+        logger.debug('fetching octokit instance and creating pull request')
         const octKit = github.getOctokit(githubToken)
 
         try {
+            logger.debug(`Creating pull request using headBranch ${{ headBranch }}`)
             await octKit.pulls.create({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
-                base: baseBranch,
+                base: headBranch,
                 head: targeBranch,
                 title: 'chore: update npm dependencies',
                 body: 'chore: this pull update npm dependencies package*.json files',
             })
         } catch (error) {
-            core.error('Error while creating pull request')
+            logger.error('Error while creating pull request')
             core.setFailed(error.message)
-            core.error(error)
+            logger.error(error)
             return
         }
 
     } else {
-        core.info('No changes detected in package*.json files')
+        logger.info('No changes detected in package*.json files')
     }
-
-    core.info('I am custom JS Action')
 }
 
 run()
