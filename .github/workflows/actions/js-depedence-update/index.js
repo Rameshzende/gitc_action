11d22b3497554c4ca74f5d99b39a32c877722c54
Rameshzende <jhande00@gmail.com>
2024-07-08 15:28:18 +0530

    code fix

--- .github/workflows/actions/js-depedence-update/index.js
+++ .github/workflows/actions/js-depedence-update/index.js
@@ -1,45 +1,45 @@
-const core = require('@actions/core');
-const exec = require('@actions/exec');
-const github = require('@actions/github');
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+
+
+const setUpGit = async () => {
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    await exec.exec(`git config --global user.email "gh-automation@email.com"`);
+}
 
 const validateBranchName = ({ branchName }) => (/^[a-zA-Z0-9_\-\.\/]+$/.test(branchName))
 const validateDir = ({ dirName }) => (/^[a-zA-Z0-9_\-\/]+$/.test(dirName))
 
-const setUpLogger = ({ debug, prefix } = {debug: false, prefix: ''}) => ({
+const setUpLogger = ({ debug, prefix } = { debug: false, prefix: '' }) => ({
     debug: (message) => {
-      if(debug) {
-        core.info(`${prefix}${prefix ? '' : 'DEBUG: '}${message}`)
-      }
+        if (debug) {
+            core.info(`DEBUG : ${prefix}${prefix ? '' : ''}${message}`)
+        }
     },
     info: (message) => {
-        core.info(`${prefix}${prefix ? '' : 'INFO: '}${message}`)
+        core.info(`INFO: ${prefix}${prefix ? '' : ''}${message}`)
     },
     error: (message) => {
-      core.error(`${prefix}${prefix ? '' : 'ERROR: '}${message}`)
+        core.error(`ERROR: ${prefix}${prefix ? '' : ''}${message}`)
     }
 })
 
-  
-const setUpGit = async () => {
-    await exec.exec('git config --global user.email "gh-automation@email.com"')
-    await exec.exec('git config --global user.name "gh-automation"')
-}
 
 
 async function run() {
-    const baseBranch = core.getInput('base-branch', { required: true })
-    const headBranch = core.getInput('head-branch', { required: true })
-    const githubToken = core.getInput('gh-token', { required: true })
-    const workingDir = core.getInput('working-directory', { required: true })
-    const debug = core.getBooleanInput('debug')
-    const logger = setUpLogger({ debug, prefix: '[js dependency update]' })
-  
+    const baseBranch = core.getInput('base-branch', { required: true });
+    const headBranch = core.getInput('head-branch', { required: true });
+    const ghToken = core.getInput('gh-token', { required: true });
+    const workingDir = core.getInput('working-directory', { required: true });
+    const debug = core.getBooleanInput('debug');
+    const logger = setupLogger({ debug, prefix: '[js-dependency-update]' });
 
     const commonExecOpts = {
         cwd: workingDir
     }
 
-    core.setSecret(githubToken)
+    core.setSecret(ghToken)
 
     logger.debug(`validating inputs: ${baseBranch}`)
 
@@ -59,7 +59,7 @@ async function run() {
     }
 
     logger.debug(`Base Branch: ${baseBranch}`)
-    logger.debug(`head Branch: ${headBranch}`)
+    logger.debug(`Head Branch: ${headBranch}`)
     logger.debug(`working directory: ${workingDir}`)
 
     logger.debug('checking for updates')
@@ -72,31 +72,42 @@ async function run() {
         {
             ...commonExecOpts,
         }
-    );
+    )
 
     if (gitStatus.stdout.length > 0) {
         logger.debug('There are updated available')
         logger.debug('Setting up git')
-        
+
         setUpGit()
-        logger.debug('Comminting and Pushing the package*.json files')
-        await exec.exec(`git checkout -b ${baseBranch}`, [], { ...commonExecOpts })
-        await exec.exec(`git add package.json package-lock.json`, [], { ...commonExecOpts })
-        await exec.exec(`git commit -m "chore: update dependencies"`, [], { ...commonExecOpts })
-        await exec.exec(`git push -u origin ${headBranch} --force`, [], { ...commonExecOpts })
-       
+  
+        logger.debug('Committing and pushing package*.json changes');
+        await exec.exec(`git checkout -b ${headBranch}`, [], {
+            ...commonExecOpts,
+        });
+        await exec.exec(`git add package.json package-lock.json`, [], {
+            ...commonExecOpts,
+        });
+        await exec.exec(`git commit -m "chore: update dependencies`, [], {
+            ...commonExecOpts,
+        });
+        await exec.exec(`git push -u origin ${headBranch} --force`, [], {
+            ...commonExecOpts,
+        });
+
         logger.debug('fetching octokit instance and creating pull request')
-        const octKit = github.getOctokit(githubToken)
+
+        const octokit = github.getOctokit(ghToken);
 
         try {
             logger.debug(`Creating pull request using headBranch ${{ headBranch }}`)
-            await octKit.pulls.create({
+
+            await octokit.pulls.create({
                 owner: github.context.repo.owner,
                 repo: github.context.repo.repo,
+                title: `Update NPM dependencies`,
+                body: `This pull request updates NPM packages`,
                 base: baseBranch,
                 head: headBranch,
-                title: 'chore: update npm dependencies',
-                body: 'chore: this pull update npm dependencies package*.json files',
             })
         } catch (error) {
             logger.error('Error while creating pull request')
